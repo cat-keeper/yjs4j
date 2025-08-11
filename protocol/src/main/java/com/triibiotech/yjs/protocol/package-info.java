557a908f340f51a449ba02a204efd5e9cc9ca0be
bangsong.zeng <zbs@tri-ibiotech.com>
2025-08-11 09:04:28 +0800

    fix: 去除无用的文件

--- protocol/src/main/java/com/triibiotech/yjs/protocol/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Yjs4j Protocol Module
- * 
- * This module contains protocol-related classes for Yjs CRDT library.
- */
-package com.triibiotech.yjs.protocol;
--- websocket/src/main/java/com/triibiotech/yjs/websocket/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Yjs4j WebSocket Module
- * 
- * This module contains WebSocket-related classes for Yjs CRDT library.
- */
-package com.triibiotech.yjs.websocket;
